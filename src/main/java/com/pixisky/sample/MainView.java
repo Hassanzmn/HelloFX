c13c4260f44c4a229c3a6a5a1349f4829ee5d2fc
sam <hassan.zmn77@gmail.com>
2025-01-13 14:25:55 +0330

    starting

---
+++ src/main/java/com/pixisky/sample/MainView.java
@@ -0,0 +1,20 @@
+package com.pixisky.sample;
+
+import com.gluonhq.charm.glisten.mvc.View;
+import java.util.ResourceBundle;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+
+public class MainView {
+
+    public View getView() {
+        try {
+            View view = FXMLLoader.load(MainView.class.getResource("main.fxml"), ResourceBundle.getBundle("com.pixisky.sample.main"));
+            return view;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+            return new View();
+        }
+    }
+
+}
